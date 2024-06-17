7e2dba52a3ee2060987bc464ab4ec70e56933601
Wesley Santos <wesley.brazil@outlook.com>
2024-06-17 18:55:51 -0300

    refactor: ordenação das constantes

--- script/src/constants.js
+++ script/src/constants.js
@@ -1,6 +1,6 @@
 export const PAGE_LIMIT = 3;
 export const PAGE_ITEMS = 10;
 export const JOB_SEARCH = ["reactjs", "node"];
-export const JOB_KEYWORDS = ["jr", "junior", "júnior", "node", "nodejs", "react", "reactjs", "fullstack", "full stack", "angular", "javascript", "js", "typescript"];
-export const JOB_KEYWORDS_EXCLUDE = ["sr", "senior", "sênior", "php", "java", ".net", "c#", "presencial", "python", "deficiência", "mid-level"];
+export const JOB_KEYWORDS = ["jr", "junior", "júnior", "node", "node.js", "nodejs", "react", "reactjs", "fullstack", "full stack", "angular", "javascript", "js", "typescript"];
+export const JOB_KEYWORDS_EXCLUDE = ["sr", "senior", "sênior", "php", "java", ".net", "c#", "presencial", "python", "deficiência", "mid-level", "pleno"];
 export const WEIRD_SITES = [];
